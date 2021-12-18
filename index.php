17af1dd11e01c3f78adf5784e715dc466f74a602
TheHersheee <95379524+TheHersheee@users.noreply.github.com>
2021-12-18 22:15:44 +0300

    First blog post

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <title>Блог TheHersheee</title>
+    </head>
+<body>
+    <h1>Блог TheHersheee</h1>
+    <hr>
+    <h2>Заметки о Git'е</h2>
+    <h6>18.12.2021</h6>
+    <p>
+        Сегодня мое альтер-эго рассказало: 
+        <pre>gitk</pre>
+        это команда для запуска программы для контроля версий, работающая на основе системы Git.
+    </p>
+    <p>
+        Команда 
+        <pre>git init</pre> 
+        позволяет создать пустой репозиторий в текущей папке.
+    </p> 
+    <p>
+        Команда 
+        <pre>git add</pre>
+        позволяет мне добавить файл в индекс.
+    </p> 
+    <p>
+        Команда 
+        <pre>git commit -m ""</pre>
+        позволяет зафиксировать измененную версию в GitK
+    </p>
+    <hr>
+    <footer>Copyright 2021 TheHersheee</footer>
+</body>
+</html>
