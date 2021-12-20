918b7ca207f08e82c4ab1d1f82dfdb2e5490c7ba
TheHersheee <95379524+TheHersheee@users.noreply.github.com>
2021-12-20 23:27:35 +0300

    Basic templayed system

---
+++ mixed.php
@@ -0,0 +1,17 @@
+<?php 
+$posts = 1;
+
+?>
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <title>Блог TheHersheee</title>
+    </head>
+<body>
+    <h1>Блог TheHersheee</h1>
+    <hr>
+    Количество моих постов - <?= $posts ?>
+    <hr>
+    <footer>Copyright © 2021 TheHersheee</footer>
+</body>
+</html>
